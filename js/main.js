8c4a54cbc52fff7b1c7118a4bf40d573f4291d7d
William Huizenga <williamhuizenga@Williams-MacBook-Air.local>
2017-06-23 11:31:39 -0400

    finished the DOM

--- js/main.js
+++ js/main.js
@@ -22,15 +22,27 @@ var cards = [
 	];
 var cardsInPlay = [];
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped "+cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
+	this.setAttribute('src',cards[cardId].cardImage);
+	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for(var x = 0; x < 4; x++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', x);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 var checkForMatch = function(){
 	if(cardsInPlay.length === 2){
@@ -43,5 +55,3 @@ var checkForMatch = function(){
 		}
 	}
 }
-
-checkForMatch();
